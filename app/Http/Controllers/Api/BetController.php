5ebcab37a97b1b716347524e76a63ef005f24d7e
UnCubanoDev <rnaranjoalbert@gmail.com>
2025-07-15 09:09:04 -0400

    Se agrega la importación del modelo Game en BetController y se elimina una importación redundante de Request. Esto mejora la claridad y organización del código en el controlador de apuestas.

--- app/Http/Controllers/Api/BetController.php
+++ app/Http/Controllers/Api/BetController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Game;
 use App\Models\Bet;
 use App\Services\BetValidationService;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class BetController extends Controller
