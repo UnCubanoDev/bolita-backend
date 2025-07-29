ff99063bc31b265716ff2253c769b6169554aadd
UnCubanoDev <rnaranjoalbert@gmail.com>
2025-07-29 13:54:15 -0400

    correccion

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/faq', function () {
     return view('faq');
 })->name('faq');
 Route::get('/download-apk', function () {
-    $file = public_path('download/lottogame.apk');
+    $file = public_path('downloads/lottogame.apk');
     return response()->download($file, 'LottoGame.apk', [
         'Content-Type' => 'application/vnd.android.package-archive',
     ]);
